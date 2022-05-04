71488bb9a61d18eb388f786bf8a58e5462336b37
MinhPythonk <phamhoangminh672@gmail.com>
2022-05-04 21:05:42 +0700

    add another type of bird for day theme

---
+++ installation/data/variables.py
@@ -0,0 +1,13 @@
+# Some useful global variables
+
+# If the bird is not colliding anything
+bird_collision = False
+
+# If the hit sound is played or not
+hit_played = False
+
+highest_score = 0
+update_score = False
+
+user_started = False
+score = 0
